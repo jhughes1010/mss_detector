ca4c219c68bfa88f65ba7fe5912e29c159c9e619
James Hughes <jhughes1010@gmail.com>
2025-03-21 05:09:09 -0600

    basic operation

--- mss_detector.ino
+++ mss_detector.ino
@@ -1,9 +1,37 @@
-void setup() {
-  // put your setup code here, to run once:
+//MSS block detector
+//ATTINY85
 
+
+#define LED PB0
+#define DETECT_IN PB1
+#define MSS_OUT PB2
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(MSS_OUT, OUTPUT);
+  digitalWrite(LED, LOW);
+  digitalWrite(MSS_OUT, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int heartbeat;
+  int occupied;
+  static long int activeTime = 0;
 
+
+  heartbeat = (millis() / 1000) % 2;
+  occupied = digitalRead(DETECT_IN);
+
+  digitalWrite(LED, heartbeat);
+
+  if(occupied){
+    activeTime=millis();
+    digitalWrite(MSS_OUT, HIGH);
+    digitalWrite(LED, HIGH);
+  }
+  else{
+    if(activeTime+400<millis()){
+      digitalWrite(MSS_OUT, LOW);
+    }
+  }
 }
