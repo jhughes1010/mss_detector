cd979284aab0fcf053ef7fd4148d7a2d006bcac6
James Hughes <jhughes1010@gmail.com>
2025-03-22 03:08:04 -0600

    mss detection polarity change

--- mss_detector.ino
+++ mss_detector.ino
@@ -1,36 +1,38 @@
 //MSS block detector
 //ATTINY85
 
-
+#define TEST_PIN PB0
 #define LED PB0
 #define DETECT_IN PB1
 #define MSS_OUT PB2
 
 void setup() {
+  pinMode(DETECT_IN, INPUT_PULLUP);
   pinMode(LED, OUTPUT);
   pinMode(MSS_OUT, OUTPUT);
   digitalWrite(LED, LOW);
   digitalWrite(MSS_OUT, LOW);
+
 }
 
 void loop() {
   int heartbeat;
   int occupied;
+  int test;
   static long int activeTime = 0;
 
 
-  heartbeat = (millis() / 1000) % 2;
-  occupied = digitalRead(DETECT_IN);
-
+  heartbeat = !((millis() / 500) % 4);
+  occupied = !digitalRead(DETECT_IN);
+  test = digitalRead(TEST_PIN);
   digitalWrite(LED, heartbeat);
 
-  if(occupied){
-    activeTime=millis();
+  if (occupied) {
+    activeTime = millis();
     digitalWrite(MSS_OUT, HIGH);
     digitalWrite(LED, HIGH);
-  }
-  else{
-    if(activeTime+400<millis()){
+  } else {
+    if (activeTime + 1000 < millis()) {
       digitalWrite(MSS_OUT, LOW);
     }
   }
